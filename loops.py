1cbf136e0f127f7f9332cb5f1121c28a2b994680
swastik <40032310+swxstikk@users.noreply.github.com>
2022-04-17 15:34:13 +1000

    Adding the first 2 files I have worked on using python.

---
+++ loops.py
@@ -0,0 +1,15 @@
+# simple program to test 'for' loop
+
+groceries=['milk', 'eggs', 'egg whites', 'pizza', 'coke', 'juice', 'salsa']
+print("the grocery list will be printed using a for loop: \n")
+for item in groceries:
+    # we can use groceries(x:y) to splice up the list
+    # where x stands for the starting position and y for the ending position
+    print(item)
+
+print('\n')
+
+supplies=['tools', 'hardware']
+print("the supplies list will be printed using a for loop too: \n")
+for equipment in supplies:
+    print(equipment)
---
+++ main.py
@@ -0,0 +1,10 @@
+# this file is used to test conditionals and print statements.
+
+name="swastik"
+age=20
+
+if name=="swastik":
+    print("what's up, Swastik?")
+else:
+    print("who are you? O.o")
+
